f88a2bf562b68c6f1033da11d190a981b6045e2b
Ryan Ward <4577045+Ward-R@users.noreply.github.com>
2025-05-18 20:34:58 +0900

    basics working. looks ugly. main post page doesnt work yet

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,36 @@
 class PostsController < ApplicationController
+
+  def new
+
+    @author = Author.find(params[:author_id])
+    @post = Post.new
+  end
+
+  def create
+    @author = Author.find(params[:author_id])
+    @post = Post.new(author_params)
+    @post.author = @author
+    if @post.save
+
+      redirect_to author_path(@post)
+    else
+      render 'authors/show', status: :unprocessable_entity
+    end
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    @post = Post.new
+  end
+
+  private
+
+  def author_params
+    params.require(:post).permit(:title, :body, :published_at)
+  end
+
 end
